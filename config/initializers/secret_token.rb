e65e9e43d8655c4ae3c12156031f31f365c3ad90
Vanessa Vengco <vengco@gmail.com>
2013-08-04 14:32:39 -0700

    New Rails install, skip active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project3::Application.config.secret_key_base = '587952c32795781e6ad7a99ccb19a865fe3855157dfd47b20c18e310bc5d01a5a871f2a3dec1bbae4f1ff9fd8e1f6278626b7c0365c31f6c64c7ed1d77e3215e'
