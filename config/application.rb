4035001fffc0c708d16a1d217237c17cc5458906
Vanessa Vengco <vengco@gmail.com>
2013-08-08 12:25:46 -0700

    Configure Rails model generator to point to mongo_mapper

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ Bundler.require(:default, Rails.env)
 
 module Project3
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.orm :mongo_mapper
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
